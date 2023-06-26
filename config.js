ea0da480f6446c2d50a4346f02f592bd35293d03
Flaaw <125206250+Flaaaw@users.noreply.github.com>
2023-06-26 14:42:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ module.exports = {
         logo: 'https://media.discordapp.net/attachments/1096698367054200846/1109501667650576446/090ef212ea9cc1240d00cef76d357122.webp?width=662&height=662',
         activity: 'vos invitations 📜',
         color: "8624875",
-        devs: ['350005907284819968', '285417432288591874']
+        devs: ['1039960600643645490', '1078675442279665667']
     }
 }
